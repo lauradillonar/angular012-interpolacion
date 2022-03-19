a42034fa889456c86c0f04f315838359356d76a3
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-19 09:36:33 -0300

    Llamando a una funcion con String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,16 @@ export class AppComponent {
     nombre: "Anders Hejisberg",
     lenguajes: ['C#', 'Delphi', 'Pascal', 'TypeScript']
   });
+
+  // Calcular si el parámetro introducido es par o impar
+  evenOrOdd = (num:number):string => {
+    if (num % 2 === 0) {
+      return num + ' es un número par';
+    } else {
+      return num + ' es un número impar';
+    }
+  }
+
+  //Muestra un número aleatorio del 1 al 10
+  numberRandom = () => (Math.floor(Math.random()*10)+1);
 }
